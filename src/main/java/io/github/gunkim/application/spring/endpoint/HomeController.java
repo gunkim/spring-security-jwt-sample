b455e4c21fb57b5797dd67d3b55f87539de4ce91
gunkim <gunkim.dev@gmail.com>
2023-03-23 22:43:21 +0900

    :truck: 패키지 이동

--- src/main/java/io/github/gunkim/application/spring/endpoint/HomeController.java
+++ src/main/java/io/github/gunkim/application/spring/endpoint/HomeController.java
@@ -1,4 +1,4 @@
-package io.github.gunkim.web;
+package io.github.gunkim.application.spring.endpoint;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
